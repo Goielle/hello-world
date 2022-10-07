d64656986c249fa01c70850c6e57ab4d0de1a1aa
Goielle <115239436+Goielle@users.noreply.github.com>
2022-10-07 15:59:39 +0200

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,1 +1,9 @@
-import 
+import pytest
+from hello_world import print_message
+
+class TestPrintMessage(object):
+  def test_
+    actual_result = print_message(-2,"Hello")
+    while pytest.raises(ValueError) as ext_info
+        
+  
