9e310623ad406c8dd073b7bfac0b9daabe4380f2
Goielle <115239436+Goielle@users.noreply.github.com>
2022-10-07 21:07:41 +0200

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -3,5 +3,7 @@ from hello_world import print_message
 
 class TestPrintMessage(object):
   def test_larger_than_zero(self):
-    while pytest.raises(ValueError) as ext_info
-      print_message(-2,"Hello")      
+    while pytest.raises(ValueError) as ext_info:
+      print_message(-2,"Hello")
+    expected_error_msg = "Argument must be >=1"
+    assert exc_info.match(expected_error_msg)
