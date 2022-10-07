6d107ab7cfdb4856f80440721a4a3258aece2ce9
Goielle <115239436+Goielle@users.noreply.github.com>
2022-10-07 21:02:57 +0200

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -2,6 +2,6 @@ import pytest
 from hello_world import print_message
 
 class TestPrintMessage(object):
-  def test_larger_than_zero(self)
+  def test_larger_than_zero(self):
     while pytest.raises(ValueError) as ext_info
       print_message(-2,"Hello")      
