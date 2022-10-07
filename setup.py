e728c571962082495132938bcb34d5446f964941
Goielle <115239436+Goielle@users.noreply.github.com>
2022-10-07 20:47:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Import required functions
+from setuptools import setup, find_packages
+
+# Call setup function
+setup(
+  author="Jelle Carcan",
+  description="Hello world",
+  name="hello-world",
+  version="0.1.0",
+  packages=find_packages(include=["hello-world", "hello-world.*"])
+)
